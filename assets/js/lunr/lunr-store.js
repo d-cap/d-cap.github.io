a86162a461eb5d230821b00ca3eac283ab2989c6
Davide Capomagi <davide.capomagi@gmail.com>
2021-02-16 06:36:43 +0000

    First article, triangle array

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Let's code it: Triangular array",
+        "excerpt":"You need to store a symmetric matrix, but you don’t want to waste space… The following should help you! We can declare a data structure that contains all the unique data to avoid the duplication and we can do some tricks with the indices to still get the correct data....","categories": ["development"],
+        "tags": ["rust","development","\"data","structure\""],
+        "url": "http://d-cap.github.io/development/2021/02/15/triangular-array.html",
+        "teaser": null
+      }]
