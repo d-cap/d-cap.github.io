4ce8e58547d3c14514f027ac9dcafac9bfbcb692
Davide Capomagi <davide.capomagi@gmail.com>
2021-02-18 08:20:35 +0000

    Updated tags and categories, added Google Ads

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "Let's code it: Triangular array",
+        "title": "Let's code it: Compact symmetric matrices",
         "excerpt":"You need to store a symmetric matrix, but you don’t want to waste space… The following should help you! We can declare a data structure that contains all the unique data to avoid the duplication and we can do some tricks with the indices to still get the correct data....","categories": ["development"],
-        "tags": ["rust","development","\"data","structure\""],
+        "tags": ["rust","development","data structure"],
         "url": "http://d-cap.github.io/development/2021/02/15/triangular-array.html",
         "teaser": null
       }]
