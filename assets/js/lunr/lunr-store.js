cd98d7f6bfdfbc4ee604fc598aa503a1362949e7
Davide Capomagi <davide.capomagi@gmail.com>
2021-03-02 12:38:34 +0000

    Added arena based tree

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,12 +2,18 @@ var store = [{
         "title": "Let's code it: Compact symmetric matrices",
         "excerpt":"You need to store a symmetric matrix, but you don’t want to waste space… The following should help you! We can declare a data structure that contains all the unique data to avoid the duplication and we can do some tricks with the indices to still get the correct data....","categories": ["development"],
         "tags": ["rust","development","data structure"],
-        "url": "http://d-cap.github.io/development/2021/02/15/triangular-array.html",
+        "url": "/development/2021/02/15/triangular-array.html",
         "teaser": null
       },{
         "title": "Let's code it: Sentence/word generations",
         "excerpt":"Are you planning to create a name generator or a sentence generator? Generate strings using Markov chains could be the easy solution. Let’s say that we have the following sentences: I really like coding I really love chatting Below you can see the chain created splitting the sentence using 3...","categories": ["development"],
         "tags": ["rust","development","algorithm"],
-        "url": "http://d-cap.github.io/development/2021/02/23/markov-chain.html",
+        "url": "/development/2021/02/23/markov-chain.html",
+        "teaser": null
+      },{
+        "title": "Let's code it: Compact tree (part 1)",
+        "excerpt":"You need a tree structure but you don’t want to pay the cost of single node allocation every time, moreover you want a structure cache friendly as much as possible? An arena based tree could be what you need. An arena based tree is a data structure that uses a...","categories": ["development"],
+        "tags": ["rust","development","data structure"],
+        "url": "/development/2021/03/02/arena-tree.html",
         "teaser": null
       }]
